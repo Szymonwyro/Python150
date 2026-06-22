217fa1fdedb6899aecd761b1a2a0cd950e6eb13c
Szymon Wyrozumski <szymwyro@icloud.com>
2026-06-22 15:25:55 +0100

    twoSum solved with pointers as sorted array allows O(n) time and O(1) space

---
+++ twoPointer/twoSumII.py
@@ -0,0 +1,11 @@
+def twoSum(self, numbers, target):
+    left = 0
+    right = len(numbers) - 1
+    while left <= right:
+        current = numbers[left] + numbers[right]
+        if current < target:
+            left += 1
+        elif current > target:
+            right -= 1
+        else:
+            return [left + 1, right + 1] #problem is 1-indexed
